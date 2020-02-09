58938b6bc6bc08cec9d84d8d41c9c5273c5839a5
Ajitesh Abhishek <ajiteshleo@gmail.com>
2020-02-09 06:24:22 -0600

    env_file added

--- test_main.py
+++ test_main.py
@@ -21,11 +21,10 @@ def client():
     yield client
 
 
-
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == False
+    assert response.json == 'Healthy'
 
 
 def test_auth(client):
